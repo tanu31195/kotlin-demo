e4a95b5902a00638de0a28eee9a9a326eacfc656
Tanushka PC <tanu31195@users.noreply.github.com>
2018-08-08 23:56:11 +0530

    Kotlin Demo Test commit

--- NullSafety.kt
+++ NullSafety.kt
@@ -10,7 +10,7 @@ fun main (args:Array<String>) {
 
     //To allow nulls, we can declare a variable as nullable string, written String?
     var name: String?
-    name = "Tanushka"
+    name = "Tanushka Bandara"
 //    name = null
 
     // !! = should have a value, cannot be null. An explicit call to throw NullPointerException()
